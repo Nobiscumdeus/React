be00335cbfb12487564d0ab62fe8b68c8bf2b9cf
Nobiscumdeus <adeolaolumide99@gmail.com>
2024-05-18 18:54:50 +0100

    updates

--- src/components/GoogleForm.jsx
+++ src/components/GoogleForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ContactForm() {
   return (
     <div className="flex flex-col justify-center items-center text-white">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSesTd7yGsBHRDsmoMxMdGIhkIpx-NBT8rJR3cDhPaD2KpX5Nw/viewform?embedded=true" width="640" height="741" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSesTd7yGsBHRDsmoMxMdGIhkIpx-NBT8rJR3cDhPaD2KpX5Nw/viewform?embedded=true" width="400" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
      
     </div>
   );
