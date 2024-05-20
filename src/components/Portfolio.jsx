ee52a25f1c5427fc4f764227e561f3f9c0d57c78
Nobiscumdeus <adeolaolumide99@gmail.com>
2024-05-20 10:13:55 +0100

    updates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { FaExternalLinkAlt } from 'react-icons/fa'; // Example icon from react-icons
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'aos/dist/aos.css';
+import imageUrl1 from './images/pexels-neo-2653362.jpg';
+import imageUrl2 from './images/banner-bg.png';
+import imageUrl3 from './images/pexels-fabricio-trujillo-60626.png';
 const PortfolioCard = ({ title, description, imageUrl, link }) => {
     AOS.init({
         easing:'ease-out-quart',
@@ -14,7 +18,7 @@ const PortfolioCard = ({ title, description, imageUrl, link }) => {
     <div data-aos="flip-right" className="max-w-sm rounded overflow-hidden shadow-lg">
 
   
-      <img className="w-full" src={imageUrl} alt={title} />
+    <img className="w-full" src={imageUrl} alt={title} />
       <div className="px-6 py-4">
         <div className=" text-fuchsia-800 font-bold text-xl mb-2">{title}</div>
         <p className="text-white text-base">{description}</p>
@@ -32,39 +36,36 @@ const PortfolioCard = ({ title, description, imageUrl, link }) => {
 };
 
 const PortfolioSection = () => {
-
   const portfolioItems = [
-    {
-      title: "Web Development",
-      description: "The Web app showcases some fully-fledged web projects created with cutting-edge web technologies ",
-      imageUrl: "portfo/images/pexels-neo-2653362.jpg",
-      link: "https://friendly-arithmetic-678f83.netlify.app/"
-    },
-    {
-      title: "Technical Writing & Blogging",
-      description: "Check out my blog posts on a range of topics which includes programming and other topics you can benefit from in no mean way.",
-      imageUrl: "../images/banner-bg.png",
-
-      link: "https://chasfatprojects.hashnode.dev"
-    },
-    // Add more portfolio items as 
-    {
-        title: "Other Projects",
-        description: "This link takes you to other cutting edge projects developed with various technologies.",
-        imageUrl: "images/pexels-fabricio-trujillo-60626.png",
-     
-        
-        link: "https://projects-a0js.onrender.com"
+      {
+          title: "Web Development",
+          description: "The Web app showcases some fully-fledged web projects created with cutting-edge web technologies ",
+          imageUrl: imageUrl1,
+          link: "https://friendly-arithmetic-678f83.netlify.app/"
+      },
+      {
+          title: "Technical Writing & Blogging",
+          description: "Check out my blog posts on a range of topics which includes programming and other topics you can benefit from in no mean way.",
+          imageUrl: imageUrl2,
+          link: "https://chasfatprojects.hashnode.dev"
+      },
+      // Add more portfolio items as needed
+      {
+          title: "Other Projects",
+          description: "This link takes you to other cutting edge projects developed with various technologies.",
+          imageUrl: imageUrl3,
+          link: "https://projects-a0js.onrender.com"
       },
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {portfolioItems.map((item, index) => (
-        <PortfolioCard key={index} {...item} />
-      ))}
-    </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {portfolioItems.map((item, index) => (
+              <PortfolioCard key={index} {...item} />
+          ))}
+      </div>
   );
 };
+
 
 export default PortfolioSection;
