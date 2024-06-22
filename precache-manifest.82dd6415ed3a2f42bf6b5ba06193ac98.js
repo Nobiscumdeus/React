d889d8b6013c4b7fb2e9523890c16da68f2946fe
Nobiscumdeus <adeolaolumide99@gmail.com>
2024-06-22 14:31:51 +0100

    Updates

---
+++ precache-manifest.82dd6415ed3a2f42bf6b5ba06193ac98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2403a9739a316a7cde05",
+    "url": "/React/static/css/main.6abc2bc1.chunk.css"
+  },
+  {
+    "revision": "2403a9739a316a7cde05",
+    "url": "/React/static/js/main.2403a973.chunk.js"
+  },
+  {
+    "revision": "ac08faf924aed8c5ffe7",
+    "url": "/React/static/js/1.ac08faf9.chunk.js"
+  },
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "66d3727950f767700f7603bb1ddb2fbc",
+    "url": "/React/index.html"
+  }
+];
